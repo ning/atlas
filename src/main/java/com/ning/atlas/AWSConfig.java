4d27f5792bd883d0b788bea77cd4549ba66e778a
Brian McCallister <brianm@skife.org>
2011-03-14 15:25:37 -0600

    initial ec2 api play

---
+++ src/main/java/com/ning/atlas/AWSConfig.java
@@ -0,0 +1,15 @@
+package com.ning.atlas;
+
+import org.skife.config.Config;
+
+public abstract class AWSConfig
+{
+    @Config("aws.access-key")
+    public abstract String getAccessKey();
+
+    @Config("aws.secret-key")
+    public abstract String getSecretKey();
+
+    @Config("aws.key-name")
+    public abstract String getKeyPairId();
+}
