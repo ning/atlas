0d74217a836e39f0d685ae7aa9f2e1e7bc963603
Brian McCallister <brianm@skife.org>
2011-04-07 10:51:34 -0600

    chef setup muckery

--- src/test/ruby/ex1/chef-server.rb
+++ src/test/ruby/ex1/chef-server.rb
@@ -1,39 +1,34 @@
 aka "ubuntu-small" => "ami-a6f504cf"
+chef_server_boot = <<-EOB
+#!/bin/sh
+# set up a chef-server
+# http://wiki.opscode.com/display/chef/Package+Installation+on+Debian+and+Ubuntu
+
+echo "deb http://apt.opscode.com/ `lsb_release -cs` main" | sudo tee /etc/apt/sources.list.d/opscode.list
+
+wget -qO - http://apt.opscode.com/packages@opscode.com.gpg.key | sudo apt-key add -
+
+sudo apt-get update
+
+sudo apt-get -y install debconf-utils
+echo "chef chef/chef_server_url string http://$(hostname -f):4000" >> /tmp/debconf-answers.conf
+echo "chef-server-webui chef-server-webui/admin_password password chef" >> /tmp/debconf-answers.conf
+echo "chef-solr chef-solr/amqp_password password chef" >> /tmp/debconf-answers.conf
+sudo debconf-set-selections < /tmp/debconf-answers.conf
+
+sudo apt-get -y install build-essential
+sudo apt-get -y install libopenssl-ruby
+sudo apt-get -y install ruby-dev
+sudo apt-get -y install libnet-ssh-multi-ruby
+sudo apt-get -y install libhighline-ruby
+
+
+# debconf preseed bits
+sudo apt-get -y install chef chef-server
+EOB
 
 
 system "chef" do
   server "chef-server", :base => "ubuntu-small",
-                   :bootstrap => <<-EOB.gsub(/^(\s*)(.*)/m, '\2').gsub(/^#{$1}/, '')
-                                   #!/bin/sh
-                                   # set up a chef-server
-                                   # http://wiki.opscode.com/display/chef/Package+Installation+on+Debian+and+Ubuntu
-
-                                   # configure the opscode apt repo
-                                   echo "deb http://apt.opscode.com/ `lsb_release -cs` main" \
-                                       | sudo tee /etc/apt/sources.list.d/opscode.list | logger
-                                   wget -qO - http://apt.opscode.com/packages@opscode.com.gpg.key | sudo apt-key add - \
-                                       | logger
-                                   sudo apt-get update  | logger
-
-                                   # set up answers to debconf questions
-                                   sudo apt-get -y install debconf-utils  | logger
-                                   echo "chef	chef/chef_server_url	string	http://$(hostname -f):4000" >> \
-                                       /tmp/debconf-answers.conf  | logger
-                                   echo "chef-server-webui	chef-server-webui/admin_password	password	chef" >> \
-                                       /tmp/debconf-answers.conf  | logger
-                                   echo "chef-solr	chef-solr/amqp_password	password	chef" >> \
-                                       /tmp/debconf-answers.conf  | logger
-                                   sudo debconf-set-selections < /tmp/debconf-answers.conf  | logger
-
-                                   # things chef likes ot have installed but doesn't explicitely depend on
-                                   sudo apt-get -y install libopenssl-ruby | logger
-                                   sudo apt-get -y install ruby-dev | logger
-                                   sudo apt-get -y install build-essential | logger
-                                   sudo apt-get -y install libnet-ssh-multi-ruby | logger
-                                   sudo apt-get -y install libhighline-ruby | logger
-                                   sudo apt-get -y install libfog-ruby | logger
-
-                                   #install chef server!
-                                   sudo apt-get -y install chef chef-server | logger
-                                 EOB
+                        :bootstrap => chef_server_boot
 end
