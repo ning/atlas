79dda487b5422a5fc89d1c18629fc0b86ad68fdc
Brian McCallister <brianm@skife.org>
2011-04-10 20:05:40 -0600

    waffly goodness!

--- src/test/ruby/base/system.rb
+++ src/test/ruby/base/system.rb
@@ -10,23 +10,21 @@ space "ec2" do
 
   bootstrapper com.ning.atlas.bootstrap.SSHBootStrapper, {
     :key_file => "~/.ec2/brianm-ning.pem",
-    :ssh_user => "ubuntu"
+    :ssh_user => "ubuntu",
+    :bastion_host => "mrpiffles@bastion.example.com"
   }
 
-  base "java-core" => { 
-    :image =>  "ami-a6f504cf", 
-    :bootstrap => "",
-    :install => "chef:galaxy"
-  }
+  base "java-core", :image =>  "ami-a6f504cf", 
+                    :bootstrap => ""
+      
 
   base "ubuntu-small" do
    image "ami-a6f504cf"
     
    bootstrap <<-EOS
       sudo apt-get -y install chef
+      chef-solo wibble wibble wibble
    EOS
-
-   install "galaxy"
   end
 
 end
