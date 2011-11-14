30581a135f66882676367c4785dfc95692e78ea2
Brian McCallister <brianm@skife.org>
2011-11-14 12:02:48 -0700

    ssh command is more useful now

--- src/main/java/com/ning/atlas/main/SSHCommand.java
+++ src/main/java/com/ning/atlas/main/SSHCommand.java
@@ -1,5 +1,7 @@
 package com.ning.atlas.main;
 
+import com.google.common.base.Joiner;
+import com.kenai.constantine.platform.Errno;
 import com.ning.atlas.Environment;
 import com.ning.atlas.Host;
 import com.ning.atlas.JRubyTemplateParser;
@@ -10,13 +12,80 @@ import com.ning.atlas.space.SQLiteBackedSpace;
 import com.ning.atlas.spi.Space;
 import com.ning.atlas.spi.protocols.SSHCredentials;
 import com.ning.atlas.spi.protocols.Server;
+import org.jruby.ext.posix.POSIX;
+import org.jruby.ext.posix.POSIXFactory;
+import org.jruby.ext.posix.POSIXHandler;
 
 import java.io.File;
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.concurrent.Callable;
 
 public class SSHCommand implements Callable<Void>
 {
     private static final Logger logger = Logger.get(SSHCommand.class);
+
+    private static final POSIX posix = POSIXFactory.getPOSIX(new POSIXHandler()
+    {
+
+        @Override
+        public void error(Errno errno, String s)
+        {
+        }
+
+        @Override
+        public void unimplementedError(String s)
+        {
+        }
+
+        @Override
+        public void warn(WARNING_ID warning_id, String s, Object... objects)
+        {
+        }
+
+        @Override
+        public boolean isVerbose()
+        {
+            return false;
+        }
+
+        @Override
+        public File getCurrentWorkingDirectory()
+        {
+            return new File(".");
+        }
+
+        @Override
+        public String[] getEnv()
+        {
+            return new String[0];
+        }
+
+        @Override
+        public InputStream getInputStream()
+        {
+            return System.in;
+        }
+
+        @Override
+        public PrintStream getOutputStream()
+        {
+            return System.out;
+        }
+
+        @Override
+        public int getPID()
+        {
+            return 0;
+        }
+
+        @Override
+        public PrintStream getErrorStream()
+        {
+            return System.err;
+        }
+
+    }, true);
 
     private final MainOptions mainOptions;
 
@@ -48,11 +117,11 @@ public class SSHCommand implements Callable<Void>
                 SSHCredentials creds = SSHCredentials.defaultCredentials(space)
                                                      .otherwise(new IllegalStateException("need to use default creds for ssh right now"));
 
-
-                System.out.printf("ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i %s %s@%s\n",
-                                  creds.getKeyFilePath(),
-                                  creds.getUserName(),
-                                  s.getExternalAddress());
+                String[] args = new String[] {
+                    // -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i %s %s@%s
+                    "/usr/bin/ssh", "-o", "UserKnownHostsFile=/dev/null", "-o", "StrictHostKeyChecking=no", "-i", creds.getKeyFilePath(), String.format("%s@%s", creds.getUserName(), s.getExternalAddress())
+                };
+                posix.execv("/usr/bin/ssh", args);
                 return null;
             }
         }
