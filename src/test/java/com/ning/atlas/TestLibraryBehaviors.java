c0cec37bf718e8dbb34b6c4acb8999bc8aae2746
Brian McCallister <brianm@skife.org>
2012-02-22 10:48:02 -0700

    some library stuff

--- src/test/java/com/ning/atlas/TestLibraryBehaviors.java
+++ src/test/java/com/ning/atlas/TestLibraryBehaviors.java
@@ -1,8 +1,16 @@
 package com.ning.atlas;
 
 import com.google.common.util.concurrent.ListenableFutureTask;
+import com.kenai.constantine.platform.Errno;
+import org.jruby.ext.posix.POSIX;
+import org.jruby.ext.posix.POSIXFactory;
+import org.jruby.ext.posix.POSIXHandler;
+import org.jruby.ext.posix.util.Finder;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
@@ -14,6 +22,71 @@ import static org.junit.Assert.assertThat;
 
 public class TestLibraryBehaviors
 {
+    @Test
+    public void testFoo() throws Exception
+    {
+        POSIX posix = POSIXFactory.getPOSIX(new POSIXHandler() {
+            @Override
+            public void error(Errno errno, String s)
+            {
+            }
+
+            @Override
+            public void unimplementedError(String s)
+            {
+            }
+
+            @Override
+            public void warn(WARNING_ID warning_id, String s, Object... objects)
+            {
+            }
+
+            @Override
+            public boolean isVerbose()
+            {
+                return false;
+            }
+
+            @Override
+            public File getCurrentWorkingDirectory()
+            {
+                return null;
+            }
+
+            @Override
+            public String[] getEnv()
+            {
+                return new String[0];
+            }
+
+            @Override
+            public InputStream getInputStream()
+            {
+                return null;
+            }
+
+            @Override
+            public PrintStream getOutputStream()
+            {
+                return null;
+            }
+
+            @Override
+            public int getPID()
+            {
+                return 0;
+            }
+
+            @Override
+            public PrintStream getErrorStream()
+            {
+                return null;
+            }
+        }, true);
+        String ssh =  Finder.findFileInPath(posix, "ssh", System.getenv("PATH"));
+        System.out.println(ssh);
+    }
+
     @Test
     public void testListenableFuture() throws Exception
     {
